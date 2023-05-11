e3458e021b5c9dc36100f95af32d13910e574e81
Sergey Shulika <developer.shulika@gmail.com>
2023-05-12 00:10:29 +0300

    init commit

---
+++ src/main/java/dev/shulika/xtelweb/XtelWebApplication.java
@@ -0,0 +1,11 @@
+package dev.shulika.xtelweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XtelWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(XtelWebApplication.class, args);
+    }
+}
