047bfd10ed4cc9abb2da7d099b276c2134af0449
Sergey Shulika <developer.shulika@gmail.com>
2023-05-16 02:26:02 +0300

    added btn(add, edit) for departments.html

--- src/main/java/dev/shulika/xtelweb/controller/DepartmentController.java
+++ src/main/java/dev/shulika/xtelweb/controller/DepartmentController.java
@@ -10,6 +10,7 @@ import org.springframework.data.web.SortDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 @RequiredArgsConstructor
@@ -29,5 +30,30 @@ public class DepartmentController {
         model.addAttribute("departments", departments);
         log.info("+++++ IN DepartmentController :: findAllDepartments :: COMPLETE +++++");
         return "departments";
+    }
+
+    @GetMapping("/departments/add")
+    public String addDepartment(Model model) {
+        log.info("+++++ IN DepartmentController :: addDepartment :: START +++++");
+        model.addAttribute("title", "Добавить отдел :: X-Tel");
+        model.addAttribute("h1", "Добавить отдел");
+
+
+        log.info("+++++ IN DepartmentController :: addDepartment :: COMPLETE +++++");
+        return "department-add";
+    }
+
+    @GetMapping("/departments/{id}/edit")
+    public String editDepartment(
+            Model model,
+            @PathVariable("id") Long id
+            ) {
+        log.info("+++++ IN DepartmentController :: editDepartment :: START +++++");
+        model.addAttribute("title", "Редактировать отдел :: X-Tel");
+        model.addAttribute("h1", "Редактировать отдел");
+
+
+        log.info("+++++ IN DepartmentController :: editDepartment :: COMPLETE +++++");
+        return "department-edit";
     }
 }
